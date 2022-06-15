dc1ed5f733eb20b8d3944b6144e22fcaca71e0bd
moy314 <moy5912@gmail.com>
2022-06-15 14:33:37 -0500

    2:creando deck

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,8 +6,34 @@
 
 //crear DECK
 
-let deck = [];
+let deck         = [];
+const tipos      = ['C','D','H','S'];
+const especiales = ['A','J','Q','K'];
 
 const crearDeck = ()=>{
 
+    for(let i = 2;i <= 10;i++){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+
+        }
+    }
+
+    //console.log(deck);
+
+
+
+for(let tipo of tipos){
+    for(let especial of especiales){
+        deck.push(tipo + especial);
+
+    }
 }
+
+console.log(deck);
+
+
+}
+
+
+crearDeck();
