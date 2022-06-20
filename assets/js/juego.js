d5f8d688fd088320a2e9158a45169c8a9084e7c8
moy314 <moy5912@gmail.com>
2022-06-20 13:31:51 -0500

    5:agregando puntaje de jugador

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -10,6 +10,22 @@ let deck         = [];
 const tipos      = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+let puntosJugador = 0,
+    puntosComputadora;
+
+
+
+//REFERENCIAS A ELEMENTOS
+
+const btnPedir = document.querySelector('#btnPedir');
+const marcadores = document.querySelectorAll('small');
+// console.log(marcadorJugador);
+//console.log(btnPedir);
+
+
+
+
+
 const crearDeck = ()=>{
 
     for(let i = 2;i <= 10;i++){
@@ -32,13 +48,14 @@ const crearDeck = ()=>{
 
     //console.log(deck);
     deck = _.shuffle(deck);
-    console.log(deck);
+     console.log(deck);
         return deck
 
 }
 
 
 crearDeck();
+
 
 
 
@@ -50,14 +67,68 @@ const pedirCarta = () =>{
         throw 'No hay cartas en el deck'
     }
     const carta = deck.pop(); 
-    console.log(deck);
-    console.log(carta);
+    // console.log(deck);
+    // console.log(carta);
     return carta;
 
 
 
 };
-deck = [];
+// VALOR CARTA
+// const valorCarta = (carta) => {
+//     let puntos = 0;
+//     const valor = carta.substring(0,carta.length -1);
+//     console.log( valor );
 
-console.log(pedirCarta());
-console.log(deck);
+//     if( isNaN( valor )){
+//         puntos = (valor === "A") ? 11 : 10;
+//         console.log("No es un numero");
+//     }else{
+//         console.log("sí es un numero");
+//         puntos = parseInt( valor )
+//     }
+//     console.log(puntos);
+    
+// }
+
+
+const valorCarta = ( carta ) =>{
+
+    const valor = carta.substring(0,carta.length -1);
+
+    return (isNaN(valor)) ? ((valor === "A") ? 11 : 10) : parseInt( valor );
+
+}
+
+// const carta = pedirCarta()
+// console.log(carta);
+// const valor = valorCarta(carta)
+// console.log(valor); 
+
+
+
+//EVENTOS
+btnPedir.addEventListener('click', () => {
+
+     const carta = pedirCarta()
+    // console.log(carta);
+    // const valor = valorCarta(carta)
+    // console.log(valor);
+    // const valor = valorCarta(pedirCarta()); 
+    puntosJugador = valorCarta(carta) + puntosJugador;
+    console.log(`los puntos acumulados son : ${puntosJugador}`);
+    marcadores[0].innerHTML = puntosJugador;
+
+
+})
+
+
+
+
+
+
+
+// deck = [];
+
+// console.log(pedirCarta());
+// console.log(deck);
